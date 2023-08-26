e9e8752ce78a1292112ce0c364a432b699248898
Srinivas <piserla.srinivas@gmail.com>
2023-08-26 19:04:34 +0530

    updated client

--- client/src/Editor.js
+++ client/src/Editor.js
@@ -27,3 +27,4 @@ export default function Editor({value,onChange}) {
     </div>
   );
 }
+
--- client/src/IndexPage.js
+++ client/src/IndexPage.js
@@ -1,10 +1,5 @@
 import Post from "./Post";
 import { useEffect , useState } from "react";
-
-
-
-
-
 function IndexPage() {
     const [posts,setPosts]=useState([]);
     useEffect(()=>{
@@ -18,10 +13,10 @@ function IndexPage() {
 
 
     },[]);
-
     return (
         <div className="">
             <h1 className='text-2xl font-bold text-center'>Posts</h1>
+            <p className='text-center'>Welcome to the blog</p>
             <div className='flex flex-col gap-3 pt-5'>
                 {posts.length > 0 && posts.map(post=><Post {...post}/>)}
             </div>
